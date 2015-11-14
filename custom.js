d7987bc50a4639fae90d283fbd5b32172ee2e6d4
soman <gao.gram@gmail.com>
2015-11-14 15:32:17 +0600

    Displaying github user info

--- custom.js
+++ custom.js
@@ -1,4 +1,22 @@
 // Code goes here
-var HelloController = function($scope) {
-  $scope.message = "Hello World!";
-};
+(function(){
+
+	var app = angular.module("GetGitHubUserInfo", []);
+
+	var HelloController = function($scope, $http) {
+
+	  var getError = function(reason) {
+	  	$scope.error = "Could not fetch github user info.";
+	  };
+
+	  var onUserSubmit = function(response) {
+	  	$scope.user = response.data;
+	  };
+
+	  $http.get("https://api.github.com/users/soman")
+	       .then(onUserSubmit, getError)
+	};
+
+	app.controller("HelloController", HelloController);
+
+}());
